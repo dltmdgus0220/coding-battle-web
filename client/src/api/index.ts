6327501c3b9b389f1b80829c7a72d68c8dfd309a
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-03-14 03:46:04 +0900

    feat: api 생성

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,1 +1,7 @@
 import axios from 'axios'; // axios: 브라우저에서 서버로 http 요청(post, get, put, delete 등)을 보내는 라이브러리
+
+const api = axios.create({
+  baseURL: 'http://localhost:4000/api', // 모든 요청의 기본 url
+  withCredentials: true,
+});
+
