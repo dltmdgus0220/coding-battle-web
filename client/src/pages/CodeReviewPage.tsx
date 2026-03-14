4b4d7c3168d1dda18cc76b6f38397a55841658f2
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-03-15 04:16:11 +0900

    feat: 객체 선언

--- client/src/pages/CodeReviewPage.tsx
+++ client/src/pages/CodeReviewPage.tsx
@@ -3,3 +3,23 @@ import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import { getSubmissionCode } from '../api'; // index.ts에 있음.
 import CodeEditor from '../components/CodeEditor';
 
+interface LocationState {
+  result: {
+    matchId: number;
+    winnerId: number | null;
+    p1: { userId: number };
+    p2: { userId: number };
+  };
+  userId: number;
+  players: {
+    player1: { id: number; username: string } | null;
+    player2: { id: number; username: string } | null;
+  };
+}
+
+interface CodeData {
+  code: string | null;
+  judge_status: string;
+  passed_cases: number;
+  total_cases: number;
+}
