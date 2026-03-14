be89ce89492928f3ff1940b8321827e34252d82c
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-03-15 07:24:39 +0900

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import LobbyPage from './pages/LobbyPage';
+import RoomPage from './pages/RoomPage';
+import ResultPage from './pages/ResultPage';
+import CodeReviewPage from './pages/CodeReviewPage';
+import './App.css';
+
